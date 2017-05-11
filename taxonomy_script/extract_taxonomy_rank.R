8f882afef8813d338db06d6cf465963c4ef8ef38
Sanjeev Sariya <sanjeevsariya@gmail.com>
2017-05-11 16:10:11 -0400

    Extract genus, species as provided by user

---
+++ taxonomy_script/extract_taxonomy_rank.R
@@ -0,0 +1,12 @@
+#
+#Find unique species or genus count
+#
+#Date May 01 2017
+#-mention rank: species, genus and this script will provide you count of unique ranks.
+
+#install.packages("argparse")
+library("argparse")
+parser <- ArgumentParser(description='Process classificaiton TSV file')
+parser$add_argument()
+
+
