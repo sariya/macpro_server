140f8b76e30cb3f5b3197428e45b9b73eea033df
Sanjeev Sariya <sanjeevsariya@gmail.com>
2017-05-12 12:08:04 -0400

    added flag for taxnmy file header. Removed fasta flag

--- rdp_core_data/find_duplicate_seqs/find_duplicateseq.py
+++ rdp_core_data/find_duplicate_seqs/find_duplicateseq.py
@@ -13,22 +13,31 @@ vsearch -cluster_fast full_derep.fasta -id 1 --sizein --sizeout --relabel OTU_  
 
 vsearch -usearch_global extracted_seq.fasta -db otus.fna -strand plus -id 1 -uc otu_table_mapping.uc
 
-Take tabtsv file generated from 
-Take fasta file
-Take taxonomy file of seqs in FASTA file
+Take tabtsv file generated from drive5 from Robert Edgar 
+Take taxonomy file of seqs with or without header. If header present then run as:
+
+#python find_duplicate_seqs/find_duplicateseq.py -o ./ -x 6_tax.txt -t tabotu_table.tsv -r #will make this variable True    
+
+else
+#python find_duplicate_seqs/find_duplicateseq.py -o ./ -x 6_tax.txt -t tabotu_table.tsv #will make this variable False      
 
 """
 import sys,os, argparse,re
 from Bio import SeqIO
 
-def get_species_name(temp_taxonomy):
+def get_species_name(temp_taxonomy,header_flag):
     """
-    take tsv taxonomy file
+    take tsv taxonomy file and flag for header. Header may be present or may not be.
     Return dictionary with seqid and and its species name 
     """
     taxonomy_dict={} #store seqid and their taxonomy  and return
     
     with open(temp_taxonomy) as tax_handle:
+
+        if header_flag==True:
+            next(tax_handle) #skip first line if header present
+        else:
+            pass
         for line in tax_handle:
             split_array=re.split('\s+',line.rstrip())
             #taxonomy_dict[split_array[0]]='\t'.join(str(x) for x in split_array[1:])
@@ -37,9 +46,10 @@ def get_species_name(temp_taxonomy):
     #--with ends
 
     return taxonomy_dict
-    #
-    #
-    #
+
+    #-----------------------------------------
+    #Function ends--<>>
+    #---------------------------------
 def get_seqids(temp_file):
     header="" #hold header
     
@@ -55,7 +65,7 @@ def get_seqids(temp_file):
     for i in range(1,len(tab_array)):
         seqid_dict[i]=tab_array[i] #store in dictionary
     #---for iteration over split tab array ends
-    #print seqid_dict
+
     return seqid_dict
     #------------------------
     #
@@ -93,17 +103,19 @@ def find_replicate_taxonomy(temp_dict_seqid,temp_tab,temp_tax_dict):
 if __name__=="__main__":
 
     parser=argparse.ArgumentParser("description")
-    parser.add_argument ('-f', '--fasta_file', help='location for fasta file',required=True) # store input fasta file
     parser.add_argument ('-o', '--out_dir', help='output direct',required=True) # store output direc
     parser.add_argument ('-t', '--tabfile', help='tab delimited otu file',required=True) # store TSV file generated from drive5
     parser.add_argument ('-x', '--tax', help='taxonomy file',required=True) # store taxonomy file
+    parser.add_argument ('-r', '--header', help='header in taxonomy, true or false',action='store_true',default=False) # store taxonomy file
     args_dict = vars(parser.parse_args()) # make them dict..
     
     taxonomy_file=os.path.abspath(args_dict['tax'])
-    fasta_file=os.path.abspath(args_dict['fasta_file'])
     tab_file=os.path.abspath(args_dict['tabfile'])
     out_dir=os.path.abspath(args_dict['out_dir'])
+    header_flag=args_dict['header'] #if header in taxonomy file present or not. Default False
+    #python find_duplicate_seqs/find_duplicateseq.py -o ./ -x 6_tax.txt -t tabotu_table.tsv -r #will make this variable True
+    #python find_duplicate_seqs/find_duplicateseq.py -o ./ -x 6_tax.txt -t tabotu_table.tsv #will make this variable False
 
     seqid_dict=get_seqids(tab_file) #gets seqid 1,2,3,4
-    tax_dict=get_species_name(taxonomy_file) #seqid and species name
+    tax_dict=get_species_name(taxonomy_file,header_flag) #seqid and species name
     find_replicate_taxonomy(seqid_dict,tab_file,tax_dict)
