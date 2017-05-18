d167ad0f0d460c26ce2ec8cefe7015035b4b1b38
Sanjeev Sariya <sanjeevsariya@gmail.com>
2017-05-18 11:24:48 -0400

    Added code for sets and length diff

--- rdp_core_data/fasta_tasks/find_comn_intsctn.py
+++ rdp_core_data/fasta_tasks/find_comn_intsctn.py
@@ -13,13 +13,20 @@ import sys,os, argparse,re
 def read_seqs_id(temp_file):
     #read text file and return list
     seq_list=[] #store seqs ids
-
+    id_set= set() #create a set and check length
     with open(temp_file) as temphandle:
         for line in temphandle:
             seq_list.append(line.rstrip())
+            id_set.add(line.rstrip())
         #-for loop ends
     #-with loop ends
+    if len(seq_list) != len(id_set):
+        print "There are duplicates present in your list. Clean the ids"
+    #-if lengths are different
+    
+    print "Length ofids in sets ",len(id_set)
     print "Seq ids found ",len(seq_list)
+
     return seq_list
     #-----------------------
     #
