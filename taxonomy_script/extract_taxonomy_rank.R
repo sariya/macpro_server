5c7aa7c60a224c01ac99a16798b33aca8db8bc23
Sanjeev Sariya <sanjeevsariya@gmail.com>
2017-05-19 10:45:34 -0400

    code with argparse

--- taxonomy_script/extract_taxonomy_rank.R
+++ taxonomy_script/extract_taxonomy_rank.R
@@ -1,12 +1,19 @@
-#
+#!/usr/bin/env Rscript
+
 #Find unique species or genus count
 #
 #Date May 01 2017
 #-mention rank: species, genus and this script will provide you count of unique ranks.
 
-#install.packages("argparse")
-library("argparse")
-parser <- ArgumentParser(description='Process classificaiton TSV file')
-parser$add_argument()
+# Run from command line Rscript  extract_taxonomy_rank.R 
+#
+
+library("argparse") #install.packages("argparse")
+parser <- ArgumentParser(description='Process classification TSV file')
+parser$add_argument('-t',"--tax",help="Provide TSV output from processing of RDP txt file")
+
+print("Helloooo")
+args <- parser$parse_args() #make it a data structure
+
 
 
