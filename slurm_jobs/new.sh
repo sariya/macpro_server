8f56b5241d1fd4267f23f807a98674203ba840da
Sanjeev Sariya <sanjeevsariya@gmail.com>
2017-06-07 10:20:30 -0400

    edited double quote

--- slurm_jobs/new.sh
+++ slurm_jobs/new.sh
@@ -4,4 +4,4 @@ printf "hello\n"
 
 #Submitting on colonial one using sbatch and command line parameter and values
 #
-# sbatch -J test_new -t 2 -p defq --wrap="bash new.sh
+# sbatch -J test_new -t 2 -p defq --wrap="bash new.sh"
